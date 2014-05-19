80508bf47b833e66f9b3f8dd8ad57d8456f929af
Sean Wesenberg <wookets@gmail.com>
2014-05-19 16:13:55 -0500

    added comments
    Signed-off-by: Sean Wesenberg <wookets@gmail.com>

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ var express = require('express');
 var jwt = require('jsonwebtoken');
 var expressJwt = require('express-jwt');
 
+// the secret is used both to 'sign' and 'verify' the jwt
 var secret = 'SecretsAreFun!!!';
 
 var app = express();
 
+// make sure we can parse incoming POST data
 app.use(require('body-parser')());
 
+// 1. we start here at the login page, fill in 'user' for the username to become authorized
 app.get('/', function(req, res, next) {
   res.sendfile('login.html');
 });
 
+// 2. authorize user based on what they pass in
 app.post('/login', function(req, res, next) {
   if(req.body.username === 'user') {
     var user = {name: 'Yo Dude'};
@@ -29,8 +33,10 @@ app.get('/api/find', function(req, res, next) {
   next()
 });
 
+// 3. verify that we have a legit jwt and decode it to the req.user field
 app.use('/api', expressJwt({secret: secret}));
 
+// 4. everything is all good, return the decoded req.user from the jwt
 app.get('/api/find', function(req, res, next) {
   res.send(req.user);
 });
